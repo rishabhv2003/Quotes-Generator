97651d2b556c1bae4b85fe4c81d5b4964e5e5fcb
Rishabh Verma <2000rishabhverma@gmail.com>
2022-09-25 13:15:51 +0530

    third commit

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const quote = document.getElementById("quote");
 const author = document.getElementById("author");
 const btn = document.getElementById('btn');
-fetch("http://api.quotable.io/random")
-        .then(res => res.json())
-        .then(data => {
-            quote.innerHTML = `" ${data.content}"`;
-            author.innerHTML = `by : ${data.author}`;
-        })
+fetch("https://api.quotable.io/random")
+    .then(res => res.json())
+    .then(data => {
+        quote.innerHTML = `" ${data.content}"`;
+        author.innerHTML = `by : ${data.author}`;
+    })
 btn.addEventListener("click", generator);
 function generator() {
-    fetch("http://api.quotable.io/random")
+    fetch("https://api.quotable.io/random")
         .then(res => res.json())
         .then(data => {
             quote.innerHTML = `" ${data.content}"`;
