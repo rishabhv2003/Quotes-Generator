c75605cd61a72d3522f59111df7137d2edc83725
Rishabh Verma <2000rishabhverma@gmail.com>
2022-09-25 19:19:18 +0530

    new api

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 const quote = document.getElementById("quote");
 const author = document.getElementById("author");
 const btn = document.getElementById('btn');
-fetch("https://api.quotable.io/random")
-    .then(res => res.json())
-    .then(data => {
-        quote.innerHTML = `" ${data.content}"`;
-        author.innerHTML = `by : ${data.author}`;
-    })
-btn.addEventListener("click", generator);
-function generator() {
-    fetch("https://api.quotable.io/random")
-        .then(res => res.json())
-        .then(data => {
-            quote.innerHTML = `" ${data.content}"`;
-            author.innerHTML = `by : ${data.author}`;
-        })
+let number = 0;
+// function to generate random numbers
+const getNewQuotes = ()=>{
+    let num = Math.floor(Math.random() * (1642 - 0) + 0);
+    number = num;
+    return num;
 }
-// fetch("https://type.fit/api/quotes")
-// .then(res => console.log(res.json()))
-// .then(data=>{
-//     quote.innerHTML = `" ${data.Array.text}"`;
-//             author.innerHTML = `by : ${data.author}`;
-// })
+generator(); // calling the function here so that div won't load blank
+btn.addEventListener("click", generator);
+let realData = "";
+// async function to generate quotes
+async function generator() {
+    const api = "https://type.fit/api/quotes";
+    try {
+        let data = await fetch(api);
+        realData = await data.json();
+        quote.innerHTML = `" ${realData[getNewQuotes()].text} "`;
+        author.innerHTML = `by : ${realData[number].author}`
+    } catch (error) {
+        alert("An error has occurred pls reload the page.")
+    }
+}
